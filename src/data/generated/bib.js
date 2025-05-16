a26757b013d8b81c25d6100c7a556b605789239d
qingjiashuijiao <yhr20001006@gmail.com>
2025-05-16 12:15:56 +0100

    第一次提交

---
+++ src/data/generated/bib.js
@@ -0,0 +1,118 @@
+﻿const generatedBibEntries = {
+    "algfoor2015": {
+        "author": "Z. A. Algfoor and M. S. Sunar and H. Kolivand",
+        "doi": "10.1155/2015/736138",
+        "journal": "International Journal of Computer Games Technology",
+        "keywords": "type:Advanced Multi-Agent Navigation",
+        "pages": "736138",
+        "title": "A Comprehensive Study on Pathfinding Techniques for Robotics and Video Games",
+        "type": "article",
+        "volume": "2015",
+        "year": "2015"
+    },
+    "barnouti2016": {
+        "author": "N. H. Barnouti and S. S. M. Al-Dabbagh and M. A. S. Naser",
+        "doi": "10.4236/jcc.2016.411002",
+        "journal": "Journal of Computer and Communications",
+        "keywords": "type:Traditional Pathfinding",
+        "number": "11",
+        "pages": "15-25",
+        "title": "Pathfinding in Strategy Games and Maze Solving Using A* Search Algorithm",
+        "type": "article",
+        "volume": "4",
+        "year": "2016"
+    },
+    "cui2010": {
+        "author": "X. Cui and H. Shi",
+        "doi": "10.5120/1288-1750",
+        "journal": "International Journal of Computer Science and Network Security",
+        "keywords": "type:Traditional Pathfinding",
+        "number": "1",
+        "pages": "125-130",
+        "title": "A*-based Pathfinding in Modern Computer Games",
+        "type": "article",
+        "volume": "11",
+        "year": "2011"
+    },
+    "harabor2012": {
+        "author": "D. Harabor and A. Grastien",
+        "booktitle": "Proc. Int. Symp. Combinatorial Search (SoCS)",
+        "doi": "10.1609/socs.v3i1.18254",
+        "keywords": "type:Traditional Pathfinding",
+        "number": "1",
+        "pages": "207-208",
+        "title": "The JPS Pathfinding System",
+        "type": "inproceedings",
+        "volume": "3",
+        "year": "2012"
+    },
+    "lawande2022": {
+        "author": "S. R. Lawande and G. Jasmine and J. Anbarasi and L. I. Izhar",
+        "doi": "10.3390/app12115499",
+        "journal": "Applied Sciences",
+        "keywords": "type:Traditional Pathfinding",
+        "number": "11",
+        "pages": "5499",
+        "title": "A Systematic Review and Analysis of Intelligence-Based Pathfinding Algorithms in the Field of Video Games",
+        "type": "article",
+        "volume": "12",
+        "year": "2022"
+    },
+    "permana2018": {
+        "author": "S. H. Permana and K. Y. Bintoro and B. Arifitama and A. Syahputra",
+        "doi": "10.30645/ijistech.v1i2.7",
+        "journal": "International Journal of Information System and Technology (IJISTECH)",
+        "keywords": "type:Traditional Pathfinding",
+        "number": "2",
+        "pages": "1",
+        "title": "Comparative Analysis of Pathfinding Algorithms A*, Dijkstra, and BFS on Maze Runner Game",
+        "type": "article",
+        "volume": "1",
+        "year": "2018"
+    },
+    "platas2025": {
+        "author": "A. Platas-L\u00f3pez and A. Guerra-Hern\u00e1ndez and M. Quiroz-Castellanos and N. Cruz-Ramirez",
+        "doi": "10.1111/exsy.13325",
+        "journal": "Expert Systems",
+        "keywords": "type:ABM + ML Integration",
+        "number": "1",
+        "pages": "e13325",
+        "title": "A Survey on Agent-Based Modelling Assisted by Machine Learning",
+        "type": "article",
+        "volume": "42",
+        "year": "2025"
+    },
+    "qiu2020": {
+        "author": "H. Qiu",
+        "doi": "10.48550/arXiv.2012.09134",
+        "journal": "arXiv preprint arXiv:2012.09134",
+        "keywords": "type:Advanced Multi-Agent Navigation",
+        "title": "Multi-agent Navigation Based on Deep Reinforcement Learning and Traditional Pathfinding Algorithm",
+        "type": "article",
+        "year": "2020"
+    },
+    "rafiq2020": {
+        "author": "A. Rafiq and T. A. A. Kadir and S. N. Ihsan",
+        "booktitle": "IOP Conference Series: Materials Science and Engineering",
+        "doi": "10.1088/1757-899X/769/1/012021",
+        "keywords": "type:Traditional Pathfinding",
+        "number": "1",
+        "pages": "012021",
+        "title": "Pathfinding Algorithms in Game Development",
+        "type": "inproceedings",
+        "volume": "769",
+        "year": "2020"
+    },
+    "zhang2021": {
+        "author": "W. Zhang and A. Valencia and N. B. Chang",
+        "doi": "10.1109/TNNLS.2021.3106777",
+        "journal": "IEEE Transactions on Neural Networks and Learning Systems",
+        "keywords": "type:ABM + ML Integration",
+        "number": "5",
+        "pages": "2170-2190",
+        "title": "Synergistic Integration Between Machine Learning and Agent-Based Modeling: A Multidisciplinary Review",
+        "type": "article",
+        "volume": "34",
+        "year": "2021"
+    }
+};
